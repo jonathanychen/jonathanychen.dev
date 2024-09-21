7e15c38c9db166fbd484be832ca54de15a92f97c
Jonathan Chen <jonathan.chenjc15243@gmail.com>
2024-09-21 04:28:16 -0400

    change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,29 @@
-import type { Metadata } from 'next'
-import { Montserrat } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
 
-const montserrat = Montserrat({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Jon Dev',
-  description: 'Portfolio',
-}
+  title: "Jonathan Chen",
+  description: "Portfolio",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={montserrat.className + " overscroll-none min-h-screen flex flex-col items-center justify-between relative"}>
+      <body
+        className={
+          montserrat.className +
+          " overscroll-none min-h-screen flex flex-col items-center justify-between relative"
+        }
+      >
         {children}
       </body>
     </html>
-  )
+  );
 }
