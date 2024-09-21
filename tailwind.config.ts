ed6a89377246357b19f25974c7180c871930f683
Jonathan Chen <jonathan.chenjc15243@gmail.com>
2024-09-20 23:33:42 -0400

    more clean up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,59 +9,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      keyframes: {
-        typing: {
-          "0%": {
-            width: "0%",
-            visibility: "hidden"
-          },
-          "100%": {
-            width: "100%"
-          }
-        },
-        intro: {
-          "0%": {
-            width: "0%",
-            visibility: "hidden"
-          },
-          "30%": {
-            width: "0%",
-          },
-          "40%": {
-            width: "20%",
-          },
-          "70%": {
-            width: "20%"
-          },
-          "100%": {
-            width: "100%"
-          }
-        },
-        blink: {
-          "50%": {
-            borderColor: "white"
-          },
-          "100%": {
-            borderColor: "transparent"
-          }
-        },
-        fadeIn: {
-          "0%": {
-            opacity: "0%",
-          },
-          "100%": {
-            opacity: "100%",
-          },
-        },
-      },
-      colors: {
-        darkblue: "#001432",
-      },
-      animation: {
-        typing: "typing 2s steps(40), blink .7s 6",
-        fadeIn: "fadeIn linear 2s",
-        intro: "intro 2.5s steps(40), blink .7s 6",
-      }
     },
   },
   plugins: [
